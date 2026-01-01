703e47f52dfb3f48b85687efc98669f00ec768e4
AmoghMerudi <amoghmerudi@gmail.com>
2026-01-01 15:51:19 -0500

    about motion

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,8 +1,14 @@
+import { motion } from "framer-motion"
+
 export default function About() {
   return (
-    <section
+    <motion.section
       id="about"
       className="min-h-screen px-6 py-24 flex items-center justify-center"
+      initial={{ opacity: 0, y: 24 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false }}
+      transition={{ duration: 0.8, ease: "easeInOut" }}
     >
       <div className="w-full max-w-6xl flex flex-col md:flex-row items-center gap-12 justify-center">
         
@@ -48,7 +54,9 @@ export default function About() {
             I enjoy coding, but I’m just as interested in design, creativity, and how people interact with the stuff they use.
             <br/>
             <br/>
-            When I’m not working on projects, I’m probably following Formula 1, taking photos, or listening to music. I like the mix of engineering, strategy, and creativity, whether that’s in software or on a race weekend. I’m always learning, always experimenting, and seeing where curiosity takes me next.
+            When I’m not working on projects, I’m probably following Formula 1, taking photos, or listening to music. 
+            I like the mix of engineering, strategy, and creativity, whether that’s in software or on a race weekend. 
+            I’m always learning, always experimenting, and seeing where curiosity takes me next.
           </p>
 
           <p className="text-white/50 text-lg mt-2">
@@ -56,6 +64,6 @@ export default function About() {
           </p>
         </div>
       </div>
-    </section>
+    </motion.section>
   )
 }
